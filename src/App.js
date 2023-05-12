f6779d153beae7828ebf01a56acc25e389845ce4
rvishwajeet9829@gmail.com <70356415+vishwajeet-singh664@users.noreply.github.com>
2023-05-12 18:28:22 +0530

    New Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import React,{useState} from 'react';
+import Rules from './Components/Rules';
+
+function App() {
+  return (
+    <div className="App">
+      <Rules></Rules>    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Rules.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+
+import React from 'react'
+
+const Rules = () => {
+
+    const [myName,setMynam]=useState('Technical')
+  return (
+
+    
+    <div>
+    <h1>{myName}</h1>
+    
+    </div> 
+  )
+}
+
+export default Rules
